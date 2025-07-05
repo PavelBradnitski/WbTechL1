7264ee4331adb9ba8ab0e8bdb87d9669901783f4
PavelBradnitski <pavel.brodnizkiy@mail.ru>
2025-07-05 13:14:55 +0300

    Add comments

--- Task1.1/main.go
+++ Task1.1/main.go
@@ -2,15 +2,18 @@ package main
 
 import "fmt"
 
+// Создаем структуру Human
 type Human struct {
 	Name string
 	Age  int
 }
 
+// Реализуем метод getHuman для структуры Human
 func (h Human) GetName() string {
 	return fmt.Sprintf("Name is: %s, Age is: %d", h.Name, h.Age)
 }
 
+// Встраиваем структуру Human
 type Action struct {
 	Human
 }
@@ -18,5 +21,6 @@ type Action struct {
 func main() {
 	h := Human{Name: "Pavel", Age: 25}
 	a := Action{Human: h}
+	// вызываем метод GetName у структуры Action
 	fmt.Println(a.GetName())
 }
