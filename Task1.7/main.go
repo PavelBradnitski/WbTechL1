c689243499eabaa07b227006d89a050ae7fceff6
PavelBradnitski <pavel.brodnizkiy@mail.ru>
2025-07-07 14:30:27 +0300

    Rename variables

--- Task1.7/main.go
+++ Task1.7/main.go
@@ -5,25 +5,25 @@ import (
 	"sync"
 )
 
-// 1. Использование sync.Mutex
-type SafeMapMutex struct {
+// Использование sync.Mutex
+type SyncMap struct {
 	data map[string]int
 	mu   sync.Mutex
 }
 
-func NewSafeMapMutex() *SafeMapMutex {
-	return &SafeMapMutex{
+func NewSyncMap() *SyncMap {
+	return &SyncMap{
 		data: make(map[string]int),
 	}
 }
 
-func (sm *SafeMapMutex) Set(key string, value int) {
+func (sm *SyncMap) Set(key string, value int) {
 	sm.mu.Lock()
 	defer sm.mu.Unlock()
 	sm.data[key] = value
 }
 
-func (sm *SafeMapMutex) Get(key string) (int, bool) {
+func (sm *SyncMap) Get(key string) (int, bool) {
 	sm.mu.Lock()
 	defer sm.mu.Unlock()
 	val, ok := sm.data[key]
@@ -31,15 +31,15 @@ func (sm *SafeMapMutex) Get(key string) (int, bool) {
 }
 
 func main() {
-	safeMapMutex := NewSafeMapMutex()
+	syncMap := NewSyncMap()
 	var wg sync.WaitGroup
 	for i := 0; i < 100; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
 			key := fmt.Sprintf("%d", i)
-			safeMapMutex.Set(key, i*2)
-			val, ok := safeMapMutex.Get(key)
+			syncMap.Set(key, i*2)
+			val, ok := syncMap.Get(key)
 			if ok {
 				fmt.Printf("Mutex: Key %s, Value %d\n", key, val)
 			}
