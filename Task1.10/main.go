530605debf55b5327f88fdd32875a228b3b9684e
PavelBradnitski <pavel.brodnizkiy@mail.ru>
2025-07-08 10:55:18 +0300

    Task 1.10 complete

---
+++ Task1.10/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var temperatures = []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	m := make(map[int][]float64)
+	var keyValue int
+	for _, v := range temperatures {
+		if v > 0 {
+			keyValue = int(math.Floor(v/10) * 10)
+		} else {
+			keyValue = int(math.Ceil(v/10) * 10)
+		}
+		val, ok := m[keyValue]
+		if !ok {
+			m[keyValue] = []float64{v}
+		} else {
+			val = append(val, v)
+			m[keyValue] = val
+		}
+	}
+	fmt.Println(m)
+}
