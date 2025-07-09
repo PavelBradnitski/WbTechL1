b7ee8273e98f4943d96f2045381245f5fa39ac7a
PavelBradnitski <pavel.brodnizkiy@mail.ru>
2025-07-09 12:01:12 +0300

    Refactor: move comments

--- Task1.16/main.go
+++ Task1.16/main.go
@@ -5,36 +5,13 @@ import "fmt"
 func main() {
 	var a = []int{1, -5, -6, 2, -1, 1, 4, 5}
 	fmt.Println(quickSort(a))
-	fmt.Println(quickSortWithoutNewArrays(a, 0, len(a)-1))
 }
 
 func quickSort(arr []int) []int {
 	if len(arr) <= 1 {
 		return arr
 	}
-	quickSortWithoutNewArrays(arr, 0, len(arr)-1)
-	return arr
-	// pivot := len(arr) / 2
-	// var left []int
-	// var right []int
-	// for i, v := range arr {
-	// 	if i == pivot {
-	// 		continue
-	// 	}
-	// 	if arr[pivot] > v {
-	// 		left = append(left, v)
-	// 	} else {
-	// 		right = append(right, v)
-	// 	}
-	// }
-	// left = quickSort(left)
-	// right = quickSort(right)
-	// var out []int
-	// out = append(out, left...)
-	// out = append(out, arr[pivot])
-	// out = append(out, right...)
-	// return out
-
+	return quickSortWithoutNewArrays(arr, 0, len(arr)-1)
 }
 
 func quickSortWithoutNewArrays(arr []int, low, high int) []int {
@@ -63,3 +40,25 @@ func partition(arr []int, low, high int) int {
 	arr[i+1], arr[high] = arr[high], arr[i+1]
 	return i + 1
 }
+
+// вариант с созданием новых массивов
+// pivot := len(arr) / 2
+// var left []int
+// var right []int
+// for i, v := range arr {
+// 	if i == pivot {
+// 		continue
+// 	}
+// 	if arr[pivot] > v {
+// 		left = append(left, v)
+// 	} else {
+// 		right = append(right, v)
+// 	}
+// }
+// left = quickSort(left)
+// right = quickSort(right)
+// var out []int
+// out = append(out, left...)
+// out = append(out, arr[pivot])
+// out = append(out, right...)
+// return out
