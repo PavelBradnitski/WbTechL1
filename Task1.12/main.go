31d7cb11f0512e22aa12c0ff846f705d9412b15d
PavelBradnitski <pavel.brodnizkiy@mail.ru>
2025-07-17 12:09:07 +0300

    Refactor to a func uniqueStrings

--- Task1.12/main.go
+++ Task1.12/main.go
@@ -4,6 +4,11 @@ import "fmt"
 
 func main() {
 	var strSlice = []string{"cat", "cat", "dog", "cat", "tree"}
+	uniqueStringsSlice := uniqueStrings(strSlice)
+	fmt.Println(uniqueStringsSlice)
+}
+
+func uniqueStrings(strSlice []string) []string {
 	// Создаем map для ускоренного поиска
 	uniqueStringsMap := make(map[string]bool)
 	for _, v := range strSlice {
@@ -14,5 +19,5 @@ func main() {
 	for key := range uniqueStringsMap {
 		uniqueStringsSlice = append(uniqueStringsSlice, key)
 	}
-	fmt.Println(uniqueStringsSlice)
+	return uniqueStringsSlice
 }
